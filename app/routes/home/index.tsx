ec30e1caa4fb64b4e3b910679fc88e326c1dec39
Eduardo Ocampo <eddie@Eduardos-MacBook-Air.local>
2025-09-29 16:00:44 -0500

    Added FeaturedProjects component. Had to add initial projects equal to empty array otherwise i get an error

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,16 +1,33 @@
 import type { Route } from './+types/index';
+import FeaturedProjects from '~/components/FeaturedProjects';
+import type { Project } from '~/types';
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: 'The Friendly Dev || Welcome' },
-    { name: 'Custom website developmet.', content: 'Welcome to React Router!' },
-  ];
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
+  const data = await res.json();
+  
+  return { projects: data };
 }
 
-export default function Home() {
+// export function meta({}: Route.MetaArgs) {
+//   return [
+//     { title: 'The Friendly Dev || Welcome' },
+//     { name: 'Custom website developmet.', content: 'Welcome to React Router!' },
+//   ];
+// }
+
+const HomePage = ({loaderData}: Route.ComponentProps) => {
+  const {projects}= loaderData;
+  // console.log(projects);
+
+
   return (
     <>
-      HomePage
+      <FeaturedProjects projects={projects} count={2}/>
     </>
   );
-}
+};
+
+export default HomePage;
