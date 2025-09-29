9504207339770d5c22aa5f0544e3b083c99005b0
Eduardo Ocampo <eddie@Eduardos-MacBook-Air.local>
2025-09-29 12:25:40 -0500

    added pagination to projectsPage

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,6 +1,8 @@
 import type { Route } from './+types/index';
 import type { Project } from '~/types';
 import ProjectCard from '~/components/ProjectCard';
+import { useState } from 'react';
+import { index } from '@react-router/dev/routes';
 
 export async function loader({
   request,
@@ -14,14 +16,50 @@ export async function loader({
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
   const { projects } = loaderData as { projects: Project[] };
 
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const projectsPerPage = 4;
+
+  // Calculate total pages
+  const totalPages = Math.ceil(projects.length / projectsPerPage);
+
+  // Get current pages projects
+
+  const indexOfLastProject = currentPage * projectsPerPage;
+  const indexOfFirstProject = indexOfLastProject - projectsPerPage;
+  const currentProjects = projects.slice(
+    indexOfFirstProject,
+    indexOfLastProject
+  );
+
+  // Pagination button render
+  const renderPagination = () => (
+    <div className='flex justify-center gap-2 mt-8 '>
+      {Array.from({ length: totalPages }, (_, index) => (
+        <button
+          className={`px-3 py-1 cursor-pointer rounded ${
+            currentPage === index + 1
+              ? 'bg-blue-600 text-white'
+              : 'bg-gray-700 text-gray-200'
+          }`}
+          onClick={() => setCurrentPage(index + 1)}
+          key={index + 1}
+        >
+          {index + 1}
+        </button>
+      ))}
+    </div>
+  );
+
   return (
     <>
       <h2 className='text-3xl text-white font-bold mb-8'>🚀 Projects</h2>
       <div className='grid gap-6 sm:grid-cols-2'>
-        {projects.map((project) => (
+        {currentProjects.map((project) => (
           <ProjectCard project={project} key={project.id} />
         ))}
       </div>
+      {totalPages > 1 && renderPagination()}
     </>
   );
 };
