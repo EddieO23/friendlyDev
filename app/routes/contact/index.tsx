f55260b0fede20f50a37d6b96131de57aae495d0
Eduardo Ocampo <eddie@Eduardos-MacBook-Air.local>
2025-09-29 16:50:51 -0500

    added form to contact page

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,13 +1,73 @@
-import React from 'react'
+import React from 'react';
 
 function ContactPage() {
   return (
-    <>
-      <h2 className="text-3xl font-bold text-white mb-8 text-center">
-        📥 Contact Me 
+    <div className='max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-700'>
+      <h2 className='text-3xl font-bold text-white mb-8 text-center'>
+        📥 Contact Me
       </h2>
-    </>
-  )
+      <form className='space-y-6'>
+        <div>
+          <label
+            htmlFor='name'
+            className='block text-sm font-medium text-white'
+          >
+            Full Name
+          </label>
+          <input
+            type='text'
+            id='name'
+            className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100'
+          />
+        </div>
+        <div>
+          <label
+            htmlFor='email'
+            className='block text-sm font-medium text-white'
+          >
+            Email
+          </label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100'
+          />
+        </div>
+        <div>
+          <label
+            htmlFor='subject'
+            className='block text-sm font-medium text-white'
+          >
+            Subject
+          </label>
+          <input
+            type='text'
+            id='subject'
+            name='subject'
+            className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100'
+          />
+        </div>
+        <div>
+          <label
+            htmlFor='message'
+            className='block text-sm font-medium text-white'
+          >
+            Message:
+          </label>
+          <textarea
+            rows={6}
+            id='message'
+            name='message'
+            className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100'
+          />
+        </div>
+        <button className='w-full bg-blue-600 hover:bg-blue-700 hover:cursor-pointer text-white py-2 rounded-lg'>
+          Send message
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default ContactPage
+export default ContactPage;
