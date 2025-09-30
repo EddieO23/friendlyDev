a8328748092be4ec54b7d036ca03e3fe40d1b45e
Eduardo Ocampo <eddie@Eduardos-Mac-mini.local>
2025-09-30 18:07:59 -0500

    added blog components.

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,8 +1,12 @@
 import type { Route } from './+types/index';
 import type { PostMeta } from '~/types';
-import { Link } from 'react-router';
 
-export async function loader({ request }: Route.LoaderArgs): Promise<{posts: PostMeta[]}> {
+
+import PostCard from '~/components/PostCard';
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
   const url = new URL('/posts-meta.json', request.url);
   const res = await fetch(url.href);
 
@@ -16,12 +20,15 @@ export async function loader({ request }: Route.LoaderArgs): Promise<{posts: Pos
 
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
   const { posts } = loaderData;
-  console.log(posts);
+  // console.log(posts);
 
   return (
-    <>
+    <div className='max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900 rounded-md'>
       <h2 className='text-3xl text-white font-bold mb-8'>📝 Blog</h2>
-    </>
+      {posts.map((post) => (
+        <PostCard key={post.slug} post={post} />
+      ))}
+    </div>
   );
 };
 
